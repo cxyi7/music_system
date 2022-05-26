50716756072966ead31c12a42d4b383cbadd2023
yoboom <1369276645@qq.com>
2022-05-26 22:41:22 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入element-plus ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置国际化
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(ElementPlus, { locale: zhCn }).use(store).use(router).mount('#app')
